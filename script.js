f1a08381df547ace0639e6467817959841a91544
Sergey Shishin <flyers32rus@gmail.com>
2023-02-07 22:59:08 +0300

    feat: add storage and render

--- script.js
+++ script.js
@@ -2,25 +2,38 @@
 
 const API_KEY = '6c19a7621696a924de1a6c588331987a';
 
-const getWeather = async () => {
-   const y = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`);
-  const getJ = await y.json();
-  console.log(getJ.main.temp);
+let weatherStorage = {
+    temp: 0,
+    city: 'Moscow'
+};
 
-  const main = document.querySelector('main')
-  const body = document.querySelector('body')
-  main.innerHTML = `
-    <div>${getJ.main.temp}</div>
-  `
-  body.append(main)
+function getCity() {
+    const inputCity = document.querySelector('.city__input');
+    const submitCity = document.querySelector('.get-weather');
+    
+    submitCity.addEventListener('click', () => {
+        weatherStorage.city = inputCity.value;
+        getCurrentWeather();
+    });
 }
 
-// getWeather()
+const getCurrentWeather = async () => {
+    const currentWeatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${weatherStorage.name}&appid=${API_KEY}&units=metric`);
+    const currentWeather = await currentWeatherResponse.json();
+    
+    weatherStorage = {
+        temp: currentWeather.main.temp
+    };
 
-function a() {
-  const take = document.querySelector('.get-weather');
-  take.addEventListener('click', () => {
-    getWeather()
-  })
+    pushElems();
+};
+
+function pushElems() {
+    const parentBlock = document.querySelector('.data');
+    const currentTemp = document.createElement('div');
+    currentTemp.classList.add('current__temp');
+    parentBlock.append(currentTemp);
 }
-a()
+
+getCity();
+
